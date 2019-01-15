7e9b45a723341c27a53dd4231fabb7ccea4d0190
i.isaev <i.isaev@pronet.ua>
2019-01-15 17:04:52 +0200

    ddd

--- sequelizeapp/models/index.js
+++ sequelizeapp/models/index.js
@@ -3,8 +3,10 @@
 var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
-var env = process.env.NODE_ENV || "development";
-var config = require(path.join(__dirname, '../../', 'config', 'config.json'))[env];
+var env = process.env.NODE_ENV;
+console.log(typeof env);
+var config = require(path.join(__dirname, '../../', 'config', 'config.json'))['db']['connections'][env];
+console.log(config);
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db = {};
 
