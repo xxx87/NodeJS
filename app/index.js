edffadc0ac912e299f9dff8e01b99ccacaa07c14
i.isaev <i.isaev@pronet.ua>
2019-02-12 16:08:14 +0200

    h

--- app/index.js
+++ app/index.js
@@ -238,8 +238,6 @@ http.createServer(function(request, response){
 	console.log("Url: " + request.url);
 	    console.log("Тип запроса: " + request.method);
 	    console.log("User-Agent: " + request.headers["user-agent"]);
-	    console.log("Все заголовки");
-	    console.log(request.headers);
 					response.setHeader("UserId", 12);
 				    response.setHeader("Content-Type", "text/html; charset=utf-8;");
 				    response.write("<h2>hello world</h2>");
